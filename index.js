7ef54ebd8c497f87f295af4a11f98e0925b914dc
FayeMD <fayemcdonald@hotmail.com>
2014-11-05 16:30:58 -0400

    hump day

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var redis = require("redis"),
+		client = redis.createClient();
+
+	client.on("error", function (err) {
+		console.log("Error " + err);
+	});
+
+	client.set("app name", "gavin-chat2", redis.print);
+	
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+	client.get('app name', function(err, reply){
+		console.log('app name is', reply);
+	});
+	client.get('last message', function(err, reply) {
+		console.log('last message', reply);
+		socket.emit('history', reply);
+	});
 	console.log('a user connected');
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
@@ -14,6 +30,7 @@ io.on('connection', function(socket){
 	socket.on('chat message', function(msg){ //listening for event
 		console.log('message: ' + msg);
 		socket.broadcast.emit('chat message', msg);
+		client.set('last message', msg);
 	});
 });
 
